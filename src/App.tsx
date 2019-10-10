fe1731397405497fb90b3c6dfa320927af751769
Benjamin Kwok <benjaminkwokhuen@gmail.com>
2019-10-11 01:35:13 +0800

    feat: create overall layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./App.css";
 import Map from "./components/Map";
+import styled from "styled-components";
+import LocationForm from "./components/LocationForm";
+import { usePathContext } from "./hooks/usePathContext";
 
-const App: React.FC = () => {
-  return <Map />;
-};
+const FlexContainer = styled.div`
+  display: flex;
+`;
+
+const App: React.FC = () => (
+  <usePathContext.Provider>
+    <FlexContainer>
+      <LocationForm />
+      <Map />
+    </FlexContainer>
+  </usePathContext.Provider>
+);
 
 export default App;
