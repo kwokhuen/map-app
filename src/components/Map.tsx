e1ee7d690b3d2892a23f9f7e919cd4ca105de673
Benjamin Kwok <benjaminkwokhuen@gmail.com>
2019-10-11 01:34:22 +0800

    feat: add polyline utilizing fetched api data

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,30 +1,44 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import GoogleMapReact from "google-map-react";
 import styled from "styled-components";
-
-type Path = [string, string][];
-
-const transformPath = (path: Path) =>
-  path.map(([lat, lng]) => ({ lat: Number(lat), lng: Number(lng) }));
-
-const defaultPath = [
-  ["22.372081", "114.107877"],
-  ["22.326442", "114.167811"],
-  ["22.284419", "114.159510"]
-] as Path;
+import { usePathContext } from "../hooks/usePathContext";
+import { transformPath } from "../utils/transformPath";
 
 const MapWrapper = styled.div`
   height: 100vh;
   width: 100%;
 `;
 
-const Map = (props: any) => {
-  const [center, setCenter] = useState({ lat: 22.285978, lng: 114.19149 });
-  const [zoom, setZoom] = useState(11);
-  const mapRef = useRef();
-  const mapsRef = useRef();
+const defaultZoom = 11;
+const defaultCenter = { lat: 22.285978, lng: 114.19149 };
 
-  console.log(process.env.REACT_APP_GOOGLE_MAP_API_KEY);
+const Map = React.memo(() => {
+  const mapRef = useRef<google.maps.Map>();
+  const mapsRef = useRef<typeof google.maps>();
+  const polylineRef = useRef<google.maps.Polyline>();
+  const { data } = usePathContext();
+
+  const handleGoogleApiLoaded = useCallback(({ map, maps }) => {
+    mapRef.current = map;
+    mapsRef.current = maps;
+  }, []);
+
+  useEffect(() => {
+    if (!mapsRef.current || !mapRef.current) return;
+    const path = data && data.path ? transformPath(data.path) : [];
+    if (!polylineRef.current) {
+      polylineRef.current = new mapsRef.current.Polyline({
+        path,
+        geodesic: true,
+        strokeColor: "#0000FF",
+        strokeOpacity: 0.8,
+        strokeWeight: 2
+      });
+      polylineRef.current.setMap(mapRef.current);
+      return;
+    }
+    polylineRef.current.setPath(path);
+  }, [data]);
 
   return (
     <MapWrapper>
@@ -32,16 +46,13 @@ const Map = (props: any) => {
         bootstrapURLKeys={{
           key: process.env.REACT_APP_GOOGLE_MAP_API_KEY
         }}
-        defaultCenter={center}
-        defaultZoom={zoom}
+        defaultCenter={defaultCenter}
+        defaultZoom={defaultZoom}
         yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={({ map, maps }) => {
-          mapRef.current = map;
-          mapsRef.current = maps;
-        }}
+        onGoogleApiLoaded={handleGoogleApiLoaded}
       />
     </MapWrapper>
   );
-};
+});
 
 export default Map;
