c1b0389b850bcd190c2f3f751506002be022d6f0
Benjamin Kwok <benjaminkwokhuen@gmail.com>
2019-10-11 01:31:08 +0800

    feat: create hook to search path from the backend

---
+++ src/hooks/useSearchPath.ts
@@ -0,0 +1,93 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { apiClient } from "../utils/apiClient";
+import { message } from "antd";
+import { usePathContext } from "./usePathContext";
+
+type Path = [string, string][];
+
+type Token = {
+  token: string;
+};
+
+type Params = {
+  origin: string;
+  destination: string;
+};
+
+type PathResponse = {
+  status: "success" | "failure" | "in progress";
+  error?: string;
+  path?: Path;
+  total_distance?: number;
+  total_time?: number;
+};
+
+export default () => {
+  const { setData } = usePathContext();
+  const [params, setParams] = useState<Params>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const isFirstRun = useRef(true);
+
+  const getToken = useCallback(
+    async () =>
+      await apiClient.post<Token>("/route", {
+        params
+      }),
+    [params]
+  );
+
+  const getPath = useCallback(async (token: Token["token"]): Promise<
+    PathResponse
+  > => {
+    const {
+      data,
+      data: { status, error }
+    } = await apiClient.get<PathResponse>(`/route/${token}`);
+    switch (status) {
+      case "success":
+        message.success("Success!");
+        return data;
+      case "failure":
+        message.error(`Sorry. ${error}.`);
+        return data;
+      case "in progress":
+        message.info(
+          "Hang in there! We're trying to retrieve your information."
+        );
+        return await getPath(token);
+      default:
+        return Promise.reject(new Error("Unknown status"));
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchResource = async () => {
+      setIsLoading(true);
+      try {
+        const {
+          data: { token }
+        } = await getToken();
+        const data = await getPath(token);
+        setData(data);
+      } catch (e) {
+        message.error("Internal server error, please try again later.");
+        setData(undefined);
+      }
+      setIsLoading(false);
+    };
+
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      return;
+    }
+
+    fetchResource();
+  }, [params]);
+
+  const fetch = useCallback((value: Params) => setParams(value), [params]);
+
+  return {
+    fetch,
+    isLoading
+  };
+};
