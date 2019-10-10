1e109f64a7aa6e39d4259652716245f0dc0f7bbf
Benjamin Kwok <benjaminkwokhuen@gmail.com>
2019-10-11 01:32:15 +0800

    feat: create context to hold path data with constate

---
+++ src/hooks/usePathContext.ts
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import createUseContext from "constate";
+
+type Path = [string, string][];
+
+type Data = {
+  status: "success" | "failure" | "in progress";
+  error?: string;
+  path?: Path;
+  total_distance?: number;
+  total_time?: number;
+};
+
+const usePath = () => {
+  const [data, setData] = useState<Data>();
+  return { data, setData };
+};
+
+export const usePathContext = createUseContext(usePath);
