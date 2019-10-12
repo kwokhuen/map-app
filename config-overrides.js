3c24eaf63f6e3975abd5bf3c83de00f7b99d3c7d
kwokhuen <benjaminkwokhuen@gmail.com>
2019-10-13 07:11:37 +0800

    chore: add compression plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,16 @@
 const { override, fixBabelImports } = require("customize-cra");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+const addCompressionPlugin = config => {
+  config.plugins.push(new CompressionPlugin());
+  return config;
+};
 
 module.exports = override(
   fixBabelImports("import", {
     libraryName: "antd",
     libraryDirectory: "es",
     style: "css"
-  })
+  }),
+  addCompressionPlugin
 );
